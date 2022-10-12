e077e293e12f4e8685a7959e6dfe2471e4053b01
buffehua <hsiaohua@outlook.com>
2022-10-12 18:26:17 +0800

    新增後台欄位資訊

--- back-station/src/views/a_booking/IndividualTent.vue
+++ back-station/src/views/a_booking/IndividualTent.vue
@@ -1,126 +1,83 @@
 <template>
+<Menu></Menu>
+<Header></Header>
 <section class="page_section">
   <div class="serch_bar">
-    <input type="text" placeholder="搜尋">
+    <input type="text" placeholder="搜尋"/>
     <button>新增</button>
     <button>修改</button>
     <button>刪除</button>
-  </div>  
+  </div>
   <table>
     <tr class="table_title">
-      <th v-for="title in titles" :class="{title_list: titleList==i}">{{title.thName}}</th>
+      <th v-for="item in titles" :key="item">{{item}}</th>
     </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
+    <tr v-for="item in orders" :key="item">
+      <td>{{item.tent_no}}</td>
+      <td>{{item.tent_style_no}}</td>
+      <td>{{item.area_no}}</td>
+      <td>{{item.tent_status}}</td>
+      <td>{{item.mem_no}}</td>
+      <td>{{item.tent_date}}</td>
+      <td>{{item.tent_year}}</td>
+      <td><button>更多</button></td>
     </tr>
   </table>
 </section>
+<Footer></Footer>
 </template>
-    
-<script>
-// @ is an alias to /src
 
+<script>
+
+import Menu from "@/components/Menu.vue";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
+name: 'HomeView',
+components: {
+  Menu,
+  Header,
+  Footer,
+},
+data() {
+  return {
+    chtName: '個別營帳管理',
+    title: '營區預訂',
+    path:'/IndvidualTent',
+    titles: [
+      '個別營帳編號',
+      '營帳類型編號',
+      '地區編號',
+      '營帳使用狀態',
+      '會員編號',
+      '建立日期',
+      '使用年限',
+    ],
+    orders: [],
+  };
+},
+methods: {
+  FetchAPIComment() {
+    fetch(``)
+      .then((response) => {
+        if (response) {
+          this.fetchError = response.status !== 200;
+          return response.json();
+        }
+      })
+      .then((responseText) => {
+        this.orders = responseText;
+      })
+      .catch((err) => {
+        this.orders = [];
+      });
   },
-  data() {
-    return {
-      chtName: '個別營帳管理',
-      title:'營區預訂',
-      titles:[
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-      ],
-      items:[       
-        {
-          lists:[
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-          ]
-        },
-      ]
-    }
+},
+created() {
+  this.FetchAPIComment();
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
