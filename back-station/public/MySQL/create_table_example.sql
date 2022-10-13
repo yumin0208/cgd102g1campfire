856338c838592ab1961f8f7983f6db461bc1d78b
LeonLu3303 <leon.lu1103@gmail.com>
2022-10-13 19:10:32 +0800

    fasdfasdf

--- back-station/public/MySQL/create_table_example.sql
+++ back-station/public/MySQL/create_table_example.sql
@@ -1,7 +1,7 @@
 
---  drop database tibamefe_cgd102g1;
---  CREATE DATABASE tibamefe_cgd102g1;
---  use tibamefe_cgd102g1;
+-- drop database tibamefe_cgd102g1;
+-- CREATE DATABASE tibamefe_cgd102g1;
+-- use tibamefe_cgd102g1;
 
 
 -- CREATE TABLE 後面加TABLE名稱 (欄位名稱_欄位類型_欄位特性) _代表空白
