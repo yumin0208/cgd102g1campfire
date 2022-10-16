c835ec1c7f72b9130b246e4a0ffbd4017c2a30bd
EstherChenGithub <maru@77yen.com>
2022-10-16 11:50:20 +0800

    註解掉刪除修改

--- back-station/src/views/f_shopping/ProductsInfo.vue
+++ back-station/src/views/f_shopping/ProductsInfo.vue
@@ -16,8 +16,8 @@
   <div class="serch_bar">
     <input type="text" placeholder="搜尋"/>
     <!-- <button>新增</button> -->
-    <router-link to="./productEdit.vue"><button>修改</button></router-link>
-    <button>刪除</button>
+    <!-- <router-link to="./productEdit.vue"><button>修改</button></router-link>
+    <button>刪除</button> -->
   </div>
   <div class="table_roll">
     <table>
